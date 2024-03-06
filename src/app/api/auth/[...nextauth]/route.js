244f28c4eda38109e8ac8b9ae03a71348646923d
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-06 20:57:58 +0700

    🌐Add scope to GoogleProvider and update error messages

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -39,13 +39,14 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      scope: ['profile', 'email'],
       profile(profile) {
         return {
           id: profile.sub,
           name: `${profile.given_name} ${profile.family_name}`,
           email: profile.email,
           image: profile.picture,
-        }
+        };
       },
     }),
   ],
--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,29 +1,34 @@
-'use server'
-import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function POST(request) {
     try {
-        const { name, password, email, phone } = await request.json()
+        const { name, password, email, phone } = await request.json();
         const hashedPassword = await bcrypt.hash(password, 10);
 
-
         const newUser = await prisma.user.create({
-            data:
-            {
+            data: {
                 name,
                 password: hashedPassword,
                 email,
-                phone
+                phone,
             },
-        })
-        return Response.json({ message: 'User created', newUser });
-    }  catch (error) {
+        });
+
+        return NextResponse.json({ message: 'User created', newUser });
+
+        
+    } catch (error) {
         if (error.code === 'P2002' && error.meta?.target?.[0] === 'email') {
-            return Response.json({ error: 'Email is already taken' }, { status: 400 });
+            return NextResponse.json({ message: 'This email is already in use' }, { status: 400 });
         }
-        return Response.json({ error: 'Internal Server Error' }, { status: 500 });
+
+        console.log(error);
+        
+    
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 500 });
     }
 }

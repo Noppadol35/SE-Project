8f2c163d2061fa3b91f3fa323ebd3149449a8d17
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-28 01:09:44 +0700

    Change statusId type to string

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -47,7 +47,7 @@ export async function POST(request: Request) {
                 
                 name,
                 capacity,          
-                statusId: Number(statusId),
+                statusId: String(statusId),
                 
             }
         })

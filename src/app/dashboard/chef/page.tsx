526dd6ad6779bf88276d85096874147c29f7b766
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-29 00:04:17 +0700

    Remove unused imports and fix code formatting in Chef page

--- src/app/dashboard/chef/page.tsx
+++ src/app/dashboard/chef/page.tsx
@@ -1,23 +1,12 @@
 
 "use client";
-import { useEffect } from "react";
-import { useSession, signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { ChefOrder } from "@/components/Dashboard/Chef/chefOrder";
 
 
 
 export default function Chef() {
-  // const { data: session, status } = useSession();
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   if (status === "unauthenticated") router.push("/login");
-  // }, [status, router]);
   return (
-    // status === "authenticated" &&
-    // session.user && (
-    <div className="">
+    <div>
       <ChefOrder />
     </div>
   );

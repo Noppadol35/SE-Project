effb966208a545dc5d4379c4ad1f994d1d23bf67
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-29 18:32:42 +0700

    Add next-auth type definitions

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,21 @@
+import { Role } from "@prisma/client";
+import type { User } from "next-auth";
+import "next-auth";
+
+type UserID = string;
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id : UserID
+        role: Role
+    }
+}
+
+declare module "next-auth" {
+    interface Session {
+        user: User &{
+            id: UserID;
+            role: Role;
+        }
+    }
+}

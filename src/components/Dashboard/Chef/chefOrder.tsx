8a0a56261e8df37767098705add8bdc24ba3311e
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-29 15:43:09 +0700

    👨🏻‍🍳 Remove unused import and code in chefOrder.tsx

--- src/components/Dashboard/Chef/chefOrder.tsx
+++ src/components/Dashboard/Chef/chefOrder.tsx
@@ -15,19 +15,3 @@ export function ChefOrder() {
   )
 }
 
-"use client"
-
-import Home from "./components/Home";
-
-export function ChefOrder() {
-  return (
-    <div className="h-full bg-base-200 ">
-      <div className="flex h-[90dvh]">
-        <div className="w-full">
-          < Home />
-        </div>
-      </div>
-    </div>
-  )
-}
-

74e9b9b53897951ace36772f17c6b858400ddf71
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-31 03:13:37 +0700

    👨🏻‍🍳 Add Navbar component to chefOrder.tsx

--- src/components/Dashboard/Chef/chefOrder.tsx
+++ src/components/Dashboard/Chef/chefOrder.tsx
@@ -1,12 +1,14 @@
 "use client"
 
+import Navbar from "@/components/Navbar";
 import Home from "./components/Home";
 
 export function ChefOrder() {
   return (
-    <div className="h-full bg-base-200 ">
+    <div className="h-full bg-base-200">
       <div className="flex h-[90dvh]">
         <div className="w-full">
+          <Navbar />
           < Home />
         </div>
       </div>

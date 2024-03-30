1fc3bb5c7cdba31ffff8a596b37101a3ae949d30
Tithipon10 <tithipon2546@gmail.com>
2024-03-31 02:28:25 +0700

    Refactor chefOrder component

--- src/components/Dashboard/Chef/chefOrder.tsx
+++ src/components/Dashboard/Chef/chefOrder.tsx
@@ -1,20 +1,3 @@
-
-"use client"
-
-import Home from "./components/Home";
-
-export function ChefOrder() {
-  return (
-    <div className="h-full bg-base-200 ">
-      <div className="flex h-[90dvh]">
-        <div className="w-full">
-          < Home />
-        </div>
-      </div>
-    </div>
-  )
-}
-
 "use client"
 
 import Home from "./components/Home";

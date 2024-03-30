3b346fbf707a4e444252a41ea51429f796228a70
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-31 02:59:46 +0700

    💣 Update getServerSession() to use authOptions

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -1,10 +1,11 @@
 import { getServerSession } from 'next-auth/next'
 import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server';
+import { authOptions } from '@/app/auth';
 
 export async function GET() {
     try {
-        const session = await getServerSession();
+        const session = await getServerSession(authOptions);
 
         return NextResponse.json({
             message: 'message',

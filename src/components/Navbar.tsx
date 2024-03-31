1db4f89b78193c687b34e61649bfc49b09fe2b55
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-31 16:18:48 +0700

    Update login button style in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
-import { buttonVariants } from "../components/ui/button";
 
 const Navbar = () => {
   return (
     <div className=" bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
       <div className="container flex items-center justify-between">
         <Link href="/">LOGO</Link>
-        
-        <Link className="w-20 bg-green-800 text-white py-2 rounded hover:bg-green-900 transition duration-300 relative text-center font-bold" href="/login">
+        <Link className="w-20 bg-green-800 text-white py-2 rounded hover:bg-green-900 transition duration-300 relative text-center font-bold uppercase" href="/login">
           login
         </Link>
       </div>

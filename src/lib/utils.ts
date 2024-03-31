72a541702fb89a0c23dc105da29f8cf4f172d9bd
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-31 16:17:59 +0700

    Fix indentation in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,19 +2,19 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
 
 export const redirectBasedOnRole = (role: string) => {
-  if (role === 'CHEF') {
-      return '/dashboard/chef'
-  } else if (role === 'MANAGER') {
-      return '/dashboard/manager'
-  } else if (role === 'WAITER') {
-      return '/dashboard/waiter'
-  } else if (role === 'CASHIER') {
-      return '/dashboard/cashier'
-  } else {
-      return '/'
-  }
+    if (role === 'CHEF') {
+        return '/dashboard/chef'
+    } else if (role === 'MANAGER') {
+        return '/dashboard/manager'
+    } else if (role === 'WAITER') {
+        return '/dashboard/waiter'
+    } else if (role === 'CASHIER') {
+        return '/dashboard/cashier'
+    } else {
+        return '/'
+    }
 }

1e97ffb6579fca272f1f58923823400c3a623817
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-04-01 15:40:48 +0700

    Refactor layout component and update imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,65 +6,65 @@ import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ColorSchemeScript, createTheme, MantineProvider } from "@mantine/core";
-import "src/app/styles/globals.css";
+import "./styles/globals.css";
 import "@mantine/core/styles.css";
 import "@mantine/dropzone/styles.css";
 import "@mantine/notifications/styles.css";
 import { SessionProvider } from "next-auth/react";
 
 type Props = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 const jost = Jost({
-  weight: ["300", "400", "500", "600", "700"],
-  style: "normal",
-  subsets: ["latin-ext"],
-  display: "swap",
+    weight: ["300", "400", "500", "600", "700"],
+    style: "normal",
+    subsets: ["latin-ext"],
+    display: "swap",
 });
 
 const theme = createTheme({
-  primaryColor: "green",
-  primaryShade: 7,
-  defaultRadius: "md",
-  fontFamily: "Jost, sans-serif",
-  headings: {
+    primaryColor: "green",
+    primaryShade: 7,
+    defaultRadius: "md",
     fontFamily: "Jost, sans-serif",
-  },
+    headings: {
+        fontFamily: "Jost, sans-serif",
+    },
 });
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchInterval: 5000,
-      refetchIntervalInBackground: true,
-      refetchOnMount: true,
-      refetchOnReconnect: true,
-      refetchOnWindowFocus: true,
+    defaultOptions: {
+        queries: {
+            refetchInterval: 5000,
+            refetchIntervalInBackground: true,
+            refetchOnMount: true,
+            refetchOnReconnect: true,
+            refetchOnWindowFocus: true,
+        },
     },
-  },
 });
 
 export default function Layout({ children }: Props) {
-  return (
-    <html className={jost.className} lang="en">
-      <head>
-        <ColorSchemeScript defaultColorScheme="light" />
-      </head>
-      <body>
-        <QueryClientProvider client={queryClient}>
-          <MantineProvider theme={theme} defaultColorScheme="light">
-            <Notifications />
-            <ModalsProvider>
-              <SessionProvider>
-                <main className=" h-screen justify-center items-center">
-                  {children}
-                </main>
-              </SessionProvider>
-            </ModalsProvider>
-          </MantineProvider>
-        </QueryClientProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html className={jost.className} lang="en">
+            <head>
+                <ColorSchemeScript defaultColorScheme="light" />
+            </head>
+            <body>
+                <QueryClientProvider client={queryClient}>
+                    <MantineProvider theme={theme} defaultColorScheme="light">
+                        <Notifications />
+                        <ModalsProvider>
+                            <SessionProvider>
+                                <main className=" h-screen justify-center items-center">
+                                    {children}
+                                </main>
+                            </SessionProvider>
+                        </ModalsProvider>
+                    </MantineProvider>
+                </QueryClientProvider>
+            </body>
+        </html>
+    );
 }

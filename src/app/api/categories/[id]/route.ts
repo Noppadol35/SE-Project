41c329a1a78ab08a674149ac2fec1135f2e560b6
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-04-02 00:55:38 +0700

    Add CRUD operations for category routes

---
+++ src/app/api/categories/[id]/route.ts
@@ -0,0 +1,58 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const categoryId = Number(params.id)
+    const categoryWithPosts = await prisma.category.findUnique({
+      where: { id: categoryId },
+      include: {
+        posts: true, // Include related posts in the response
+      },
+    })
+    return Response.json(categoryWithPosts)
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { name } = await req.json()
+    const category = await prisma.category.update({
+      where: { id: Number(params.id) },
+      data: { name },
+    })
+    return Response.json(category)
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    return Response.json(
+      await prisma.category.delete({
+        where: { id: Number(params.id) },
+      })
+    )
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}
---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function GET() {
+  try {
+    const categories = await prisma.category.findMany()
+    return Response.json(categories)
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { name } = await req.json()
+    const newcategories = await prisma.category.create({
+      data: {
+        name
+      },
+    })
+    return Response.json(newcategories)
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}

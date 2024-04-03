095423d9679c6047a6e87442e965c580b2d5bdf0
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-04-03 13:36:42 +0700

    Fix updata cart Btn components

--- src/app/menu/[id]/components/Tab.tsx
+++ src/app/menu/[id]/components/Tab.tsx
@@ -2,218 +2,231 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Link from "next/link"
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-  Paper,
-  Button,
-  IconButton,
-} from '@mui/material';
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+    Paper,
+    Button,
+    IconButton,
+} from "@mui/material";
 
-
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
-
-
-
-
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 const StickyHeadTable = ({ params }: { params: { id: string } }) => {
-  const router = useRouter();
-  const { id } = params;
-  
-  const [menu, setMenu] = useState([]);
-  
-  const [quantity, setQuantity] = useState('');
-  const [menuID, setMenuID] = useState("");
-  const [cart, setcartID] = useState("");
-  const [category, setCategory] = useState("");
-  const [categories, setCategories] = useState([])
-  const [quantities, setQuantities] = useState([])
-  const [search, setSearch] = useState('')
-  const [sort, setSort] = useState('name');
-  const [name, setname] = useState("");
-  const [isInCart, setIsInCart] = useState(false);
+    const router = useRouter();
+    const { id } = params;
 
-  
-  const fetchPostsTable = async (id: string) => {
-    try {
-      const res = await axios.get(`http://localhost:3000/api/posts/${id}`);
+    const [menu, setMenu] = useState([]);
 
-      setname(res.data.name || "");
-      
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const [quantity, setQuantity] = useState("");
+    const [menuID, setMenuID] = useState("");
+    const [cart, setcartID] = useState("");
+    const [category, setCategory] = useState("");
+    const [categories, setCategories] = useState([]);
+    const [quantities, setQuantities] = useState([]);
+    const [search, setSearch] = useState("");
+    const [sort, setSort] = useState("name");
+    const [name, setname] = useState("");
+    const [isInCart, setIsInCart] = useState(false);
+    const [isInCartMap, setIsInCartMap] = useState<{
+        [menuID: number]: boolean;
+    }>({});
 
-  const fetchMenu = async () => {
-    try {
-      const query = new URLSearchParams({search, category}).toString()
-      const response = await axios.get(`http://localhost:3000/api/menu?${query}`)
-      setMenu(response.data);
-      
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  const fetchCategories = async () => {
-    try {
-      const response = await axios.get(`http://localhost:3000/api/categories`);
-      setCategories(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  const fetchCart = async () => {
-    try {
-      const response = await axios.get(`/api/Cart?tableID=${id}`);
-      const cartData = response.data;
-      const isItemInCart = cartData.some((item: any) => item.status === 'waiting to send..');
-      setIsInCart(isItemInCart);
-      setcartID(cartData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
- 
-  const handleApplyFilters = () => {
-    fetchMenu();
-    
-  }
+    const fetchPostsTable = async (id: string) => {
+        try {
+            const res = await axios.get(
+                `http://localhost:3000/api/posts/${id}`
+            );
 
-  const buttonClass = isInCart
-  ? "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
-  : "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full";
+            setname(res.data.name || "");
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
+    const fetchMenu = async () => {
+        try {
+            const query = new URLSearchParams({ search, category }).toString();
+            const response = await axios.get(
+                `http://localhost:3000/api/menu?${query}`
+            );
+            setMenu(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const fetchCategories = async () => {
+        try {
+            const response = await axios.get(
+                `http://localhost:3000/api/categories`
+            );
+            setCategories(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const fetchCart = async () => {
+        try {
+            const response = await axios.get(`/api/Cart?tableID=${id}`);
+            const cartData = response.data;
+            const isItemInCart = cartData.some(
+                (item: any) => item.status === "waiting to send.."
+            );
+            setIsInCart(isItemInCart);
+            setcartID(cartData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  useEffect(() => {
-    fetchPostsTable(id);
-    fetchMenu();
-    fetchCategories();
-    fetchCart();
-  }, [id]);
+    const handleApplyFilters = () => {
+        fetchMenu();
+    };
 
-  const handleAddToCart = async (menuId: number) => {
-    try {
-      const response = await axios.get(`/api/Cart?menuID=${menuId}&tableID=${id}`);
-      const isItemInCart = response.data.some((item: any) => item.status === 'waiting to send..');
-  
-      if (isItemInCart) {
-        
-        const itemToRemove = response.data.find((item: any) => item.status === 'waiting to send..');
-        await axios.delete(`/api/Cart/${itemToRemove.id}`);
-        
-      } else {
-        
-        await axios.post('/api/Cart', {
-          menuID: menuId,
-          quantity: 1,
-          tableID: id,
-          status: 'waiting to send..', // เพิ่มสถานะ 'waiting to send' ให้กับรายการใหม่
-        });
-        // แสดงข้อความแจ้งเตือนหรือทำการอื่น ๆ หลังจากเพิ่มรายการเสร็จสิ้น
-      }
-      fetchCart(); // เรียกฟังก์ชัน fetchCart หลังจากเพิ่มหรือลบรายการในตะกร้า
-    } catch (error) {
-      console.error('Error adding/removing to/from cart:', error);
-    }
-  };
-  
+    const buttonClass = isInCart
+        ? "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+        : "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full";
 
+    useEffect(() => {
+        fetchPostsTable(id);
+        fetchMenu();
+        fetchCategories();
+        fetchCart();
+    }, [id]);
 
+    const handleAddToCart = async (menuId: number) => {
+        try {
+            const response = await axios.get(`/api/Cart?tableID=${id}`);
+            const cartData = response.data;
 
-  return (
-    <div >
-      <Grid container justifyContent="center">
-        <Grid item xs={12} md={8}>
-        <div className="flex justify-center items-center mb-6 py-3">
-                <div className="flex gap-1">
-                  <input
-                    type="text"
-                    placeholder="Search by name.."
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  />
-                  <select
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                    className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  >
-                    <option value="">Select Categories</option>
-                    {categories.map((cat: any) => (
-                      <option key={cat.id} value={cat.name}>
-                        {cat.name}
-                      </option>
-                    ))}
-                  </select>
-                  
-                  <button
-                    onClick={handleApplyFilters}
-                    className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition-colors focus:outline-none"
-                  >
-                    Apply
-                  </button>
-                 
-                </div>
-              </div>
-             
-          <TableContainer component={Paper}>
+            // หาว่ารายการนี้อยู่ในตะกร้าหรือไม่
+            const itemInCart = cartData.find(
+                (item: any) =>
+                    item.menuID === menuId &&
+                    item.status === "waiting to send.."
+            );
 
-            <Table stickyHeader aria-label="sticky table">
-              
-           
-              <TableHead>
-                <TableRow>
-                  <TableCell>NAME</TableCell>
-                  <TableCell align="left">CATEGORY</TableCell>
-                  <TableCell align="center">QUANTITY</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {menu.map((menu: any) => (
-                  <TableRow key={menu.id}>
-                    <TableCell >
-                      <Typography variant="body1">{menu.name}</Typography>
-                    </TableCell>
-                    <TableCell  align="left">
-                      <Typography variant="body1">{menu.category.name}</Typography>
-                    </TableCell>
-                    <TableCell  align="center">
-                        <Box sx={{ minWidth: 120 }}>
-                          <Button
-                            variant="contained"
-                            className={buttonClass}
-                            onClick={() => handleAddToCart(menu.id)}
-                          >
-                            {isInCart ? 'Remove from Cart' : 'Add to Cart'}
-                          </Button>
-                        </Box>
-                      </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-      </Grid>
-    </div>
-  );
+            if (itemInCart) {
+                // ถ้ารายการนี้อยู่ในตะกร้า ให้ลบออก
+                await axios.delete(`/api/Cart/${itemInCart.id}`);
+                setIsInCart(false); // กำหนดให้ isInCart เป็น false สำหรับรายการนี้
+            } else {
+                // ถ้ารายการนี้ไม่อยู่ในตะกร้า ให้เพิ่มเข้าไป
+                await axios.post("/api/Cart", {
+                    menuID: menuId,
+                    quantity: 1,
+                    tableID: id,
+                    status: "waiting to send..",
+                });
+                setIsInCart(true); // กำหนดให้ isInCart เป็น true สำหรับรายการนี้
+            }
+
+            fetchCart(); // เรียกฟังก์ชัน fetchCart หลังจากเพิ่มหรือลบรายการในตะกร้า
+        } catch (error) {
+            console.error("Error adding/removing to/from cart:", error);
+        }
+    };
+
+    return (
+        <div>
+            <Grid container justifyContent="center">
+                <Grid item xs={12} md={8}>
+                    <div className="flex justify-center items-center mb-6 py-3">
+                        <div className="flex gap-1">
+                            <input
+                                type="text"
+                                placeholder="Search by name.."
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                                className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            />
+                            <select
+                                value={category}
+                                onChange={(e) => setCategory(e.target.value)}
+                                className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            >
+                                <option value="">Select Categories</option>
+                                {categories.map((cat: any) => (
+                                    <option key={cat.id} value={cat.name}>
+                                        {cat.name}
+                                    </option>
+                                ))}
+                            </select>
+
+                            <button
+                                onClick={handleApplyFilters}
+                                className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition-colors focus:outline-none"
+                            >
+                                Apply
+                            </button>
+                        </div>
+                    </div>
+
+                    <TableContainer component={Paper}>
+                        <Table stickyHeader aria-label="sticky table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>NAME</TableCell>
+                                    <TableCell align="left">CATEGORY</TableCell>
+                                    <TableCell align="center">
+                                        QUANTITY
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {menu.map((menu: any) => (
+                                    <TableRow key={menu.id}>
+                                        <TableCell>
+                                            <Typography variant="body1">
+                                                {menu.name}
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <Typography variant="body1">
+                                                {menu.category.name}
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Box sx={{ minWidth: 120 }}>
+                                                <Button
+                                                    variant="contained"
+                                                    className={
+                                                        isInCartMap[menu.id]
+                                                            ? "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+                                                            : "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+                                                    }
+                                                    onClick={() =>
+                                                        handleAddToCart(menu.id)
+                                                    }
+                                                >
+                                                    {isInCartMap[menu.id]
+                                                        ? "Remove from Cart"
+                                                        : "Add to Cart"}
+                                                </Button>
+                                            </Box>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default StickyHeadTable;
